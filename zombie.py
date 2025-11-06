edc62c796cb18e6846750c6f58e736a3cf0ef0f0
황상준 <hsj45935@gmail.com>
2025-11-06 22:01:25 +0900

    좀비 크기에 따라 바운딩 박스도 크기 조절되도록 수정

--- zombie.py
+++ zombie.py
@@ -37,7 +37,7 @@ class Zombie:
 
 
     def get_bb(self):
-        return self.x - 100, self.y - 100, self.x + 100, self.y + 100
+        return self.x - self.size // 2, self.y - self.size // 2, self.x + self.size // 2, self.y + self.size // 2
 
     def handle_collision(self, group, other):
         if group == 'boy:zombie':
